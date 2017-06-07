7dddc10718c1a62de8bda37ef2393f0e7e6d6e0b
DaoDuyTuan <tuan123coi96@gmail.com>
2017-06-07 23:12:33 +0700

    ExceptionC.java
    discuss to Exception

---
+++ ExceptionC.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg11.pkg17;
+
+/**
+ *
+ * @author phong.th
+ */
+public class ExceptionC extends ExceptionB {
+    public ExceptionC() {
+    }
+    
+    public ExceptionC(String message) {
+        super(message);
+    }
+}
